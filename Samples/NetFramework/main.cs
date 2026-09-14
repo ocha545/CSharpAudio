3e9a05f2bf091f5c83787cb238d930ab5f3ba50a
ocha-- <140893815+ocha545@users.noreply.github.com>
2026-09-14 16:07:51 +0900

    add: サンプル用のファイルとコードを追加しました。

---
+++ Samples/NetFramework/main.cs
@@ -0,0 +1,31 @@
+using System;
+using CSA;
+using CSA.Types;
+using CSA.Types.Format;
+
+
+class Program
+{
+	public static void Main()
+	{
+		CSharpAudio csa = new CSharpAudio();
+
+		// シャイニングスター : 魔王魂(MaouDamashii)
+		MP3 mp3 = new MP3("../maou_14_shining_star.mp3");
+
+		int handle = csa.Submit(mp3.GetBuffer(), mp3.GetInfo(), 0);
+		if(handle == -1)
+		{
+			Console.WriteLine("音声ファイルが読み込めませんでした");
+			return;
+		}
+		
+
+		Console.WriteLine("再生中...");
+		csa.Start(handle);
+		while(csa.IsStarting(handle))
+		{
+		}
+		Console.WriteLine("再生終了");
+	}
+}
